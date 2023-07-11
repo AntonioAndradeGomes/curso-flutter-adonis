8e5ae1c2a0db9bb5378c3193296993961f73d733
AntonioAndradeGomes <gomesmax1997@gmail.com>
2023-07-11 20:56:03 -0300

    aula 27

--- api-hotfruti/app/Models/Product.ts
+++ api-hotfruti/app/Models/Product.ts
@@ -1,19 +1,40 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from "luxon";
+import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
 
 export default class Product extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
 
   @column()
   public name: string;
 
   @column()
-  public description: string;
+  public description: string | null;
+
+  @column()
+  public position: number;
+
+  @column()
+  public active: boolean;
+
+  @column()
+  public image: string;
+
+  @column()
+  public price: number;
+
+  @column()
+  public unit: string;
+
+  @column()
+  public category_id: number;
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt: DateTime;
+
+  @column.dateTime()
+  public deletedAt: DateTime | null;
 }
